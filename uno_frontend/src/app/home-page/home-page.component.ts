388513e217276625dfa41b8178ea51c8c5fc7db3
MetroSCK04 <xn8506ej@go.minnstate.edu>
2023-03-20 20:02:54 -0500

    Initial commit and push
    edited files according to professors notes

---
+++ uno_frontend/src/app/home-page/home-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent {
+  HomePageTitle:string = 'This is the title for the Home Page'
+}
